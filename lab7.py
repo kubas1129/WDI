2c6a259fa08b68d505f8f4c5e0dba49381d1afdd
kubas1129 <kubas11513@gmail.com>
2017-11-29 00:24:51 +0100

    working on sorting

--- lab7.py
+++ lab7.py
@@ -5,6 +5,8 @@ Created on Mon Nov 20 08:41:57 2017
 @author: palkjaku
 """
 
+ceny = {1:1,2:3}
+
 def primeEratostenes(n):
     if (n < 2): return -1
     primes = [i+1 for i in range(n)]  #init tablicy liczb
@@ -27,33 +29,37 @@ def primeEratostenes(n):
 
     
 
-
 def removePrimeNumber(lista):
     lista.sort()
     checkingNumber=lista[0]
     count=0
     wynikowa = ""
-    for i in range(len(lista)-1):
-        if(lista[i] == 1):
-            wynikowa += "1 "
-            count = 1
-            continue
+    primes = primeEratostenes(lista[len(lista)-1])
+    for i in range(len(lista)):
         if(checkingNumber == lista[i]):
             count += 1
         else:
             if(count % 2 != 0):
-                if (not(lista[i] in primeEratostenes(lista[len(lista) - 1]))):
+                if (not(checkingNumber in primes)):
                     wynikowa += count * (str(checkingNumber) + " ")
             else:
                 wynikowa += count * (str(checkingNumber) + " ")
             checkingNumber = lista[i]
             count = 1
+        if(i == len(lista)-1):
+            if (count % 2 != 0):
+                if (not (checkingNumber in primeEratostenes(lista[len(lista) - 1]))):
+                    wynikowa += count * (str(checkingNumber) + " ")
+            else:
+                wynikowa += count * (str(checkingNumber) + " ")
+
     return wynikowa
 
 
 
+
+
 def main():
-    print(primeEratostenes(100))
     print(removePrimeNumber([5,7,4,18,3,7,5,1,2,12,7,13]))
     
 main()
---
+++ lab8.py
@@ -0,0 +1,39 @@
+import random
+
+
+def initTable(size):
+    return [random.randint(0,100) for i in range(size)]
+
+def sortByChoosed(numbers):
+    for i in range(len(numbers)):
+        minimum=i
+        for j in range(i,len(numbers)):
+            if(numbers[j] < numbers[minimum]):
+                minimum=j
+        numbers[i],numbers[minimum]=numbers[minimum],numbers[i]
+    return numbers
+
+
+def sortByInsert(numbers):
+    sort = []
+    sort.append(numbers[-1])
+    for e in range(len(numbers)-2,-1,-1):
+        for i in range(len(sort)):
+           if(numbers[e] <= sort[i]):
+               sort.insert(i,numbers[e])
+               break
+           elif(i == len(sort)-1):
+               sort.append(numbers[e])
+
+    return sort
+
+
+def bubbleSort(numbers):
+
+
+
+def main():
+    #print(sortByChoosed(initTable(100)))
+    #print(sortByInsert([100,20,1,87,76,2,9,34,22]))
+
+main()
